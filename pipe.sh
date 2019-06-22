0b8a4a537d08d983f771b7abc54e40f825550120
Sam Albon <sam.albon@enthusiastgaming.com>
2019-06-21 22:53:09 -0400

    Changed environment variable names to better match commonly used patterns

--- pipe.sh
+++ pipe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get login for AWS CLI
-$(aws ecr get-login --no-include-email --region $AWS_REGION)
+$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
 # Tag and push the image to ECR
-docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${AWS_ECR_URL}/${IMAGE_NAME}:${IMAGE_TAG}
-docker push ${AWS_ECR_URL}/${IMAGE_NAME}:${IMAGE_TAG}
+docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_URL}/${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${ECR_URL}/${IMAGE_NAME}:${IMAGE_TAG}

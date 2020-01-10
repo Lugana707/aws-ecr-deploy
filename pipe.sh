ffd0aba9539e07cf253eeb85654c44257a0b87f3
Sam Albon <sam.m.albon@outlook.com>
2020-01-10 12:15:40 -0500

    Moved AWS ECR login to before we build the image
    This will allow people to build their new images using private images from their own AWS ECR.

--- pipe.sh
+++ pipe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Build the docker image
-docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKER_FILE ./
-
 # Get login for AWS CLI
 $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+
+# Build the docker image
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKER_FILE ./
 
 # Tag and push the image to ECR
 docker tag $IMAGE_NAME:$IMAGE_TAG $ECR_URL/$IMAGE_NAME:$IMAGE_TAG
